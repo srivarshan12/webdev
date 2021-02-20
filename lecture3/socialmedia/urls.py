eeb8dc93ea1edc4e25ffd5bebb994b96f17a5333
srivarshan12 <64324102+srivarshan12@users.noreply.github.com>
2021-02-20 22:34:22 +0530

    Delete lecture3 directory

--- lecture3/socialmedia/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns=[
-    path("",views.index, name="index"),
-    # url(r'^my_def_in_view$', views.my_def_in_view, name='my_def_in_view'),
-]

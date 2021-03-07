daf36ebee6c92ac9aef6b1174ce1a46ddb017278
srivarshan12 <varshan1206@gmail.com>
2021-03-07 21:11:35 +0530

    loopstudious-landing-page

--- ablog/theblog/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import *
-# Register your models here.
-
-admin.site.register(post)
-admin.site.register(Category)
-admin.site.register(profile)
-
-admin.site.register(comment)

0f65ecb4723cdc0bd0c58775c8229c7ee0a7c2ed
srivarshan12 <varshan1206@gmail.com>
2021-03-13 13:32:56 +0530

    portfolio file updated

---
+++ my own portfolio/script.js
@@ -0,0 +1,34 @@
+
+$(document).ready(function(){
+    // Preloader
+    preloaderFadeOutTime = 500;
+    function hidePreloader() {
+        var preloader = $('.spinner-wrapper');
+        preloader.fadeOut(preloaderFadeOutTime);
+        }
+    hidePreloader();
+
+// my work -filter
+    $('.individuallinks').click(function(){
+        const value=$(this).attr('data-filter');
+        if(value=='all'){
+            $('.itembox').show('2000');
+        }
+        else{
+            $('.itembox').not('.'+value).hide('2000');
+            $('.itembox').filter('.'+value).show('2000');
+
+        }
+    })
+    
+// mywork link
+    $('.individuallinks').click(function(){
+        $(this).addClass('active').siblings().removeClass('active');
+    })
+
+// navbar-link 
+    $('.navitems-div').click(function(){
+        $(this).addClass('active-navitems').siblings().removeClass('active-navitems');
+    })
+
+})

804f5e67f04ac33a989cf529e4ff87f5790db7b6
srivarshan12 <varshan1206@gmail.com>
2021-07-29 01:19:58 +0530

    added mobile version

--- manage-landing-page-master/script.js
+++ manage-landing-page-master/script.js
@@ -1,17 +1,57 @@
+function myFunction(x) {
+  var hamburger=document.getElementById("hamburger");
+  var close=document.getElementById("close");
+  var navbar=document.getElementById("navbar");
+
+  if (x.matches) { // If media query matches
+    // document.body.style.backgroundColor = "yellow";
+    hamburger.style.display="block";
+    close.style.display="none";
+    navbar.style.display="none";
+    $('.testimonials-carousel').slick({
+      slidesToShow: 3,
+      centerMode: true,
+      variableWidth:true,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      arrows:false,  
+      dots:true
+    });
+   
+
+
+  } else {
+  //  document.body.style.backgroundColor = "pink";
+    hamburger.style.display="none";
+    close.style.display="none";
+    navbar.style.display="flex";
+    $('.testimonials-carousel').slick({
+      slidesToShow: 3,
+      centerMode: true,
+      variableWidth:true,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      arrows:false,
+      dots:false
+    });
+   
+  }
+}
+
+var x = window.matchMedia("(max-width: 425px)")
+myFunction(x) // Call listener function at run time
+x.addListener(myFunction) // Attach listener function on state changes
 
 
 $(document).ready(function(){
   // Slick version 1.5.8
-    $('.testimonials-carousel').slick({
-    slidesToShow: 3,
-    centerMode: true,
-    variableWidth:true,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    arrows:false
-  });
-  });
+     });
   
+
+
+
+
+
 
   function checkemail(){
         var email=document.getElementById("checkemail");
@@ -20,10 +60,37 @@ $(document).ready(function(){
 
         if(!filter.test(email.value)){
             email_errormsg.style.display="block";
+
             email.value='';
         }
         else{
             email_errormsg.style.display="none";
             email.value="";
         }
+  }
+
+
+
+
+  function opennavbar(){
+    var hamburger=document.getElementById("hamburger");
+    var close=document.getElementById("close");
+    var navbar=document.getElementById("navbar");
+
+    navbar.style.display="flex";
+    hamburger.style.display="none";
+    close.style.display="block";
+
+  }
+
+
+  function closenavbar(){
+    console.log("close clicked");
+    var hamburger=document.getElementById("hamburger");
+    var close=document.getElementById("close");
+    var navbar=document.getElementById("navbar");
+    
+    navbar.style.display="none";
+    close.style.display="none";
+    hamburger.style.display="block";
   }
